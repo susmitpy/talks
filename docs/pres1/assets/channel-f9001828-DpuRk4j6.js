d6d17a2a0a89d0d3ca26210a4150c7a95105adc4
Susmit Vengurlekar <susmit.py@gmail.com>
2024-08-04 13:55:40 +0530

    github actions
    Signed-off-by: Susmit Vengurlekar <susmit.py@gmail.com>

--- docs/pres1/assets/channel-f9001828-DpuRk4j6.js
+++
@@ -1,1 +0,0 @@
-import{aP as o,aQ as r}from"./slidev/md-Wzw8EEBN.js";const s=(a,n)=>o.lang.round(r.parse(a)[n]),e=s;export{e as c};
--- docs/template/assets/slidev/default-Oa9K7dXk.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-Db2xmFnq.js";import{_ as r}from"../index-C3QpmEM7.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
