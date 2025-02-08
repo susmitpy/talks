361f5e6611078710b3d6a32fe3c8883c5d89ec04
Susmit Vengurlekar <susmit.py@gmail.com>
2025-02-08 09:42:33 +0530

    update
    Signed-off-by: Susmit Vengurlekar <susmit.py@gmail.com>

--- global-bottom.vue
+++ global-bottom.vue
@@ -2,7 +2,7 @@
 <template>
     <footer
       v-if="$nav.currentPage !== 3"
-      class="absolute bottom-0 left-0 right-0 p-2 text-xs">
-      Susmit Vengurlekar - <a href="https://x.com/susmitpy" target="_blank">@susmitpy</a>
+      class="absolute bottom-0 left-0 right-0 p-2 text-s">
+      Susmit Vengurlekar - <a href="https://x.com/susmitpy" target="_blank">@susmitpy</a> #mumtechmeetup #jan25mtp
     </footer>
 </template>
